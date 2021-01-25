564aa920c5cda29051bb81f76e076d9749a9b9fb
unknown <kweperera93111@gmail.com>
2021-01-25 22:10:38 +0530

    last commit

--- src/main/java/com/satellite_api_master/satellite_catalog/model/Catalog.java
+++ src/main/java/com/satellite_api_master/satellite_catalog/model/Catalog.java
@@ -2,7 +2,7 @@ package com.satellite_api_master.satellite_catalog.model;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "satellite_catalog",catalog = "catalogdb")
+@Table(name = "satellite_catalog",catalog = "satellitedb")
 public class Catalog {
 
     @Id
